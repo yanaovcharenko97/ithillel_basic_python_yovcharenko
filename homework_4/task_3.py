692dc09aece9b665e4aac1976caa144cc4a9c5e9
yana ovcharenko <yana.ovcharenko.97@gmail.com>
2023-02-07 15:56:18 +0300

    initial commit

---
+++ homework_4/task_3.py
@@ -0,0 +1,5 @@
+number = input('    Please enter an integer number:\n ')
+number = int(number)
+
+print(f'    Next number for number {number} is\n {number + 1}.')
+print(f'    Previous number for number {number} is\n {number + 1}.')
