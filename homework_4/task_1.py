e3d7cfad7894a3b1432d8d325a36f9d6f9831b54
yana ovcharenko <yana.ovcharenko.97@gmail.com>
2023-02-08 13:04:47 +0300

    initial commit

--- homework_4/task_1.py
+++ homework_4/task_1.py
@@ -1,3 +1,3 @@
-text = input('Please enter text: ')
+text = input('Please enter text (in format world_world_world): ')
 
 print('\nResult:', text.split('_')[0].capitalize() + text.split('_')[1].capitalize() + text.split('_')[2].capitalize())
--- homework_4/task_3.py
+++ homework_4/task_3.py
@@ -2,3 +2,4 @@ number = int(input('    Please enter an integer number:\n'))
 
 print(f'    Next number for number {number} is\n{number + 1}.\n'
       f'    Previous number for number {number} is\n{number - 1}.')
+      
