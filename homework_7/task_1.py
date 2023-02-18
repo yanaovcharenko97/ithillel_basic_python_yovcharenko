237460a3775efa8c9061eafc90600f407ff868a1
yana ovcharenko <yana.ovcharenko.97@gmail.com>
2023-02-19 01:57:24 +0300

    initial commit

--- homework_7/task_1.py
+++ homework_7/task_1.py
@@ -1,24 +1,38 @@
-print("To find out if the chess knight can be on the desired square of the chessboard in one move, please enter:\n")
+def chess_knight(x1_letter, y1_number, x2_letter, y2_number):
+    letter_designations = "abcdefgh"
+    x1_numer = 0
+    for i in range(len(letter_designations)):
+        if letter_designations[i] == x1_letter[0]:
+            x1_numer = i
 
-x1_letter, y1_number = input("the square from which the knight starts its move - ")
-x2_letter, y2_number = input("the square where the knight should go - ")
+    x2_numer = 0
+    for i in range(len(letter_designations)):
+        if letter_designations[i] == x2_letter[0]:
+            x2_numer = i
 
-letter_designations = "abcdefgh"
+    dx = abs(x1_numer - x2_numer)
+    dy = abs(int(y1_number) - int(y2_number))
 
-x1_numer = 0
-for i in range(len(letter_designations)):
-    if letter_designations[i] == x1_letter[0]:
-        x1_numer = i
+    if dy == 2 and dx == 1 or dx == 2 and dy == 1:
+        return True
+    else:
+        return False
 
-x2_numer = 0
-for i in range(len(letter_designations)):
-    if letter_designations[i] == x2_letter[0]:
-        x2_numer = i
 
-dx = abs(x1_numer - x2_numer)
-dy = abs(int(y1_number) - int(y2_number))
+def main():
+    print("To find out if the chess knight can be on the desired square "
+          "of the chessboard in one move, please enter:")
+    
+    x1_letter, y1_number = input("the square from which the knight starts its move - ")
+    x2_letter, y2_number = input("the square where the knight should go - ")
+    
+    result = chess_knight(x1_letter, y1_number, x2_letter, y2_number)
+    
+    if result is True:
+        print("\nYES. The knight can go to this square in one move.")
+    else:
+        print("\nNO. The knight cannot go to this square in one move.")
 
-if dy == 2 and dx == 1 or dx == 2 and dy == 1:
-    print('\nYES. The knight can go to this square in one move.')
-else:
-    print('\nNO. The knight cannot go to this square in one move.')
+
+if __name__ == "__main__":
+    main()
