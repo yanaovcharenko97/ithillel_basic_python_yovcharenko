d70b07182fa590437050d20b18997df9518f7d69
yana ovcharenko <yana.ovcharenko.97@gmail.com>
2023-02-18 14:40:00 +0300

    initial commit

--- homework_2/task_4.py
+++ homework_2/task_4.py
@@ -1,4 +1,5 @@
 from decimal import *
 number = Decimal(input('Введіть кількість гривенень: '))
 exchange = Decimal('37.44')
-print(f"Станом на 30 січня 2023 року це становить {number * exchange} доларів США")
+result = number / exchange
+print(f"Станом на 30 січня 2023 року це становить", round(result, 2), "доларів США")
