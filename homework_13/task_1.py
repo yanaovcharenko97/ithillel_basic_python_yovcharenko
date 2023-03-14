a115e95360325e9bceb75e592fb153372f939f1c
yana ovcharenko <yana.ovcharenko.97@gmail.com>
2023-03-14 05:50:26 +0300

    initial commit

--- homework_13/task_1.py
+++ homework_13/task_1.py
@@ -1,3 +1,42 @@
+"""
+This module implements a Godzilla class that can eat humans and keep track of its stomach capacity.
+It also includes a function to get integer input and a main function that allows the user to
+simulate feeding Godzilla with different humans.
+
+Classes:
+    Godzilla
+The Godzilla class represents a creature that can eat humans and keep track of its stomach capacity.
+
+    Attributes:
+stomach_capacity (int): The maximum amount of food the Godzilla's stomach can hold.
+stomach_content (int): The current amount of food in the Godzilla's stomach.
+
+    Methods:
+eat(human_size): Tries to eat a human of a given size. If the human is smaller than the
+remaining capacity of the stomach, it is eaten and the stomach content is increased
+by the human's size. Otherwise, the human is not eaten and a message is printed.
+
+    Functions:
+
+    get_int_input(prompt: str) -> int:
+This function takes a string prompt as an argument and repeatedly asks the user to input an integer
+until a valid integer is entered. It returns the integer.
+
+    main() -> None:
+This function simulates feeding Godzilla with different humans. It first asks the user to enter the
+stomach capacity of Godzilla. Then, it repeatedly asks the user to enter the size of a human to feed
+Godzilla until the user enters 0 to finish. It calls the eat() method of the Godzilla object with the
+human size as the argument. If the stomach is almost full, it prints a warning message. Finally, it
+asks the user if they want to play again.
+
+    test_godzilla():
+This function contains a series of tests to verify that the Godzilla class is working correctly.
+It checks that Godzilla can eat people of different sizes, that she does not exceed her stomach
+capacity, and that the warning feature that Godzilla is getting full works correctly.
+Calling the test_godzilla() function will help verify that everything is working correctly.
+"""
+
+
 class Godzilla:
     def __init__(self, stomach_capacity):
         self.stomach_capacity = stomach_capacity
@@ -6,24 +45,70 @@ class Godzilla:
     def eat(self, human_size):
         if self.stomach_content + human_size <= self.stomach_capacity:
             self.stomach_content += human_size
-            print(f"~ Godzilla ate a human of size, {human_size} ~")
+            print(f"~ Godzilla ate a human of size {human_size} ~")
             if self.stomach_content >= self.stomach_capacity * 0.9:
                 print("~ Godzilla is getting full! ~")
         else:
             print("~ Godzilla can't eat anymore, the stomach is full! ~")
 
 
-def main():
-    stomach_capacity = int(input("Enter Godzilla's stomach capacity: "))
-    godzilla = Godzilla(stomach_capacity)
+def test_godzilla():
+    # Test eating a human smaller than stomach capacity
+    godzilla = Godzilla(10)
+    godzilla.eat(5)
+    assert godzilla.stomach_content == 5
 
+    # Test eating a human larger than stomach capacity
+    godzilla.eat(10)
+    assert godzilla.stomach_content == 5
+
+    # Test eating a human that makes the stomach almost full
+    godzilla.eat(4)
+    assert godzilla.stomach_content == 9
+
+    # Test eating a human that fills the stomach to capacity
+    godzilla.eat(1)
+    assert godzilla.stomach_content == 10
+
+    # Test eating a human when the stomach is already full
+    godzilla.eat(2)
+    assert godzilla.stomach_content == 10
+
+    # Test eating multiple humans
+    godzilla = Godzilla(15)
+    godzilla.eat(5)
+    godzilla.eat(7)
+    godzilla.eat(4)
+    assert godzilla.stomach_content == 16
+
+    print("All tests pass")
+
+
+def get_int_input(prompt: str):
     while True:
-        human_size = int(input("\nEnter the size of a human to feed Godzilla (or enter 0 to finish): "))
-        if human_size == 0:
+        try:
+            value = int(input(prompt))
+            return value
+        except ValueError:
+            print("Invalid input. Please enter an integer.")
+
+
+def main():
+    while True:
+        stomach_capacity = get_int_input("Enter Godzilla's stomach capacity: ")
+        godzilla = Godzilla(stomach_capacity)
+
+        while True:
+            human_size = get_int_input("\nEnter the size of a human to feed "
+                                       "Godzilla (or enter 0 to finish): ")
+            if human_size == 0:
+                break
+            godzilla.eat(human_size)
+
+        play_again = input("\nDo you want to play again? (y/n) ")
+        if play_again.lower() != 'y':
             break
-        godzilla.eat(human_size)
 
 
 if __name__ == '__main__':
     main()
-
--- homework_13/task_2.py
+++ homework_13/task_2.py
@@ -1,44 +1,86 @@
+"""
+This module defines a Circle class with a nested Point class. The Circle class
+represents a circle with a center point and a radius. The Point class represents
+a point in a two-dimensional plane with x and y coordinates.
+
+Classes:
+Circle: A class representing a circle with a center point and a radius.
+Point: A class representing a point in a two-dimensional plane with x and y coordinates.
+
+Functions:
+test_circle() -> None: A function to test the Circle class's is_inside method.
+get_float_input(prompt: str) -> float: A function to prompt the user for a float input.
+main() -> None: A function to prompt the user for input to create a Circle object and
+a Point object and determine whether the point is inside the circle.
+
+Usage:
+To test the Circle class, call the test function.
+To use the Circle class to determine whether a point is inside a circle, call the main function.
+
+Example:
+
+Create a Circle object
+c = Circle(Point(0, 0), 5)
+
+Create a Point object
+p = Point(3, 4)
+
+Check if the point is inside the circle
+result = c.is_inside(p)
+
+Print the result
+print(result) # Output: True
+"""
+
 from math import sqrt
 
 
-class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-
 class Circle:
-    def __init__(self, center, radius):
+    class Point:
+        def __init__(self, x: float, y: float) -> None:
+            self.x = x
+            self.y = y
+
+    def __init__(self, center: Point, radius: float) -> None:
         self.center = center
         self.radius = radius
 
-    def is_inside(self, point):
+    def is_inside(self, point: Point) -> bool:
         distance = sqrt((point.x - self.center.x) ** 2 + (point.y - self.center.y) ** 2)
         return distance < self.radius
 
 
-def test():
-    p = Point(1, 1)
-    c = Circle(Point(1, 1), 1)
+def test_circle():
+    p = Circle.Point(1, 1)
+    c = Circle(Circle.Point(1, 1), 1)
     result = c.is_inside(p)
     print(result)
     assert result is True
 
-    p = Point(2, 2)
-    c = Circle(Point(1, 1), 1)
+    p = Circle.Point(2, 2)
+    c = Circle(Circle.Point(1, 1), 1)
     result = c.is_inside(p)
     print(result)
     assert result is False
 
 
+def get_float_input(prompt: str) -> float:
+    while True:
+        try:
+            value = float(input(prompt))
+            return value
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+
+
 def main():
-    x = float(input("Enter x-coordinate of point: "))
-    y = float(input("Enter y-coordinate of point: "))
-    p = Point(x, y)
-    x_center = float(input("\nEnter x-coordinate of circle center: "))
-    y_center = float(input("Enter y-coordinate of circle center: "))
-    radius = float(input("Enter circle radius: "))
-    c = Circle(Point(x_center, y_center), radius)
+    x = get_float_input("Enter x-coordinate of point: ")
+    y = get_float_input("Enter y-coordinate of point: ")
+    p = Circle.Point(x, y)
+    x_center = get_float_input("\nEnter x-coordinate of circle center: ")
+    y_center = get_float_input("Enter y-coordinate of circle center: ")
+    radius = get_float_input("Enter circle radius: ")
+    c = Circle(Circle.Point(x_center, y_center), radius)
     result = c.is_inside(p)
 
     if result is True:
@@ -49,4 +91,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-    
