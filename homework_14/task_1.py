63ff15152220fc1d18dc75f841d974d06418043c
yana ovcharenko <yana.ovcharenko.97@gmail.com>
2023-03-25 18:38:29 +0300

    modified commit

--- homework_14/task_1.py
+++ homework_14/task_1.py
@@ -5,80 +5,95 @@ It also includes a function to get an integer distance input from the user to
 calculate the travel time and cost for that distance on different
 types of transport.
 """
+from enum import Enum
 
 
 class Transport:
+    """Parent class representing basic properties of a mode of transport
+    and calculating travel time.
     """
-    The transport class is a parent class that describes the basic properties
-    of the transport and calculates the time to overcome the distance
-    """
-    kind_of_transport = "Transport"
 
-    def __init__(self, speed, count_of_seats):
+    kind_of_transport = "Transport"
+    extra_travel_time = 0
+
+    def __init__(self, speed: float, count_of_seats: int) -> None:
+        """Initializes a new instance of the Transport class.
+
+        Args:
+        speed (float): The speed of the transport in km/h.
+        count_of_seats (int): The number of seats in the transport.
+        """
         self.speed = speed
         self.count_of_seats = count_of_seats
 
-    def display_info(self):
-        """ Displays the main parameters of the vehicle """
-        print(f"Kind of transport: {self.kind_of_transport}")
-        print(f"Speed: {self.speed} km/h")
-        print(f"Count of seats: {self.count_of_seats}")
+    def display_info(self) -> str:
+        """Returns a string containing the main parameters of the transport.
 
-    def calculate_travel_time(self, distance):
+        Returns:
+        str: A string containing the transport's kind, speed,
+        and count of seats.
         """
-        The parameter takes the distance for which it calculates the
-        time to overcome it in hours and minutes
+        info = []
+        info.append(f"Kind of transport: {self.kind_of_transport}")
+        info.append(f"Speed: {self.speed} km/h")
+        info.append(f"Count of seats: {self.count_of_seats}")
+        return "\n".join(info)
+
+    def calculate_travel_time(self, distance: float) -> float:
+        """Calculates the time it will take to travel a given distance
+        at the transport's speed.
+
+        Args:
+        distance (float): The distance to be traveled.
+        Returns:
+        float: The time it will take to travel the distance in minutes.
         """
-        travel_time = (distance / self.speed) * 60
-        hours = travel_time // 60
-        minutes = travel_time % 60
-        return f"{round(hours)}h {round(minutes)}m"
+        travel_time = (distance / self.speed + self.extra_travel_time) * 60
+        return travel_time
+
+
+class WagonType(Enum):
+    """A simple enumeration representing different types of wagons."""
+    compartment = {'ticket_price': 0.05}
+    sitting = {'ticket_price': 0.03}
 
 
 class Train(Transport):
+    """The train class is a child class of transport and inherits its
+    attributes and methods, and also has additional attributes and a method for
+    calculating the cost of the trip.
     """
-    The train class is a child class of transport and inherits its attributes
-    and methods, and also has additional attributes and a method for
-    calculating the cost of the trip
-    """
-    kind_of_transport = "Train"
 
-    def __init__(self, speed, count_of_wagons=10, wagon_type=None,
-                 count_seats_per_wagon=40, bed_linen_cost=0):
+    kind_of_transport = "Train"
+    extra_travel_time = 0.5   # add 30 minutes for stops
+
+    def __init__(self, speed: float, count_of_wagons: int = 10,
+                 wagon_type: str = None, count_seats_per_wagon: int = 40,
+                 bed_linen_cost: float = 0) -> None:
         super().__init__(speed, count_seats_per_wagon * count_of_wagons)
         self.count_of_wagons = count_of_wagons
         self.wagon_type = wagon_type
         self.count_seats_per_wagon = count_seats_per_wagon
         self.bed_linen_cost = bed_linen_cost
 
-    def get_travel_cost(self, distance):
+    def get_travel_cost(self, distance: float) -> float:
+        """Calculates the cost of the trip, taking into account the class of the wagon.
+
+        Args:
+            distance (float): The distance to be traveled.
+        Returns:
+            float: The cost of the trip in dollars.
         """
-        Calculates the cost of the trip, taking into account the class of the wagon.
-        """
-        if self.wagon_type == "compartment":
-            # approximate ticket price per kilometer for compartment
-            ticket_price = 0.05
-        elif self.wagon_type == "sitting":
-            # approximate ticket price per kilometer for sitting car
-            ticket_price = 0.03
+
+        for wagon in WagonType:
+            if self.wagon_type == wagon.name:
+                total_cost_travel = wagon.value['ticket_price'] * distance \
+                                    + self.bed_linen_cost
+                break
         else:
             total_cost_travel = 0
-            return total_cost_travel
-
-        total_cost_travel = ticket_price * distance + self.bed_linen_cost
 
         return round(total_cost_travel, 2)
-
-    def calculate_travel_time(self, distance):
-        """
-        The parameter accepts a distance for which the time to overcome
-        it in hours and minutes is calculated for this type of transport
-        """
-        # add 30 minutes for stops
-        travel_time = (distance / self.speed + 0.5) * 60
-        hours = travel_time // 60
-        minutes = travel_time % 60
-        return f"{round(hours)}h {round(minutes)}m"
 
 
 class Airplane(Transport):
@@ -88,89 +103,149 @@ class Airplane(Transport):
     calculation of the cost of the trip, as well as informs about the
     additional cost of baggage
     """
+
     kind_of_transport = "Airplane"
+    extra_travel_time = 1   # add 1 hour for check-in and security
+    airline_surcharge = 3
 
     def __init__(self, speed, count_of_seats, fuel_cost_for_one_km,
-                 cost_per_kilogram_of_baggage):
+                 cost_per_kilogram_of_baggage) -> None:
+        """
+        Initializes the attributes of the Airplane object.
+
+        Args:
+        speed (int): The speed of the airplane in kilometers per hour.
+        count_of_seats (int): The number of seats in the airplane.
+        fuel_cost_for_one_km (float): The fuel cost for one kilometer of travel.
+        cost_per_kilogram_of_baggage (float): The cost per kilogram of baggage.
+        """
         super().__init__(speed, count_of_seats)
         self.fuel_cost_for_one_km = fuel_cost_for_one_km
         self.cost_per_kilogram_of_baggage = cost_per_kilogram_of_baggage
 
-    def get_travel_cost(self, distance):
+    def get_travel_cost(self, distance) -> float:
         """
         Calculates the cost of the trip, taking into account the cost of fuel
         and the airline's surcharge.
+
+        Args:
+        distance (float): The distance of the trip in kilometers.
+        Returns:
+        float: The cost of the trip.
         """
-        total_cost_travel = ((self.fuel_cost_for_one_km * distance) / self.count_of_seats) * 3
+        total_cost_travel = ((self.fuel_cost_for_one_km * distance) /
+                             self.count_of_seats) * self.airline_surcharge
 
         return round(total_cost_travel, 2)
 
-    def cost_kilogram_of_baggage(self):
-        """ informs about the additional cost of baggage """
-        print(f"The price of one kg of luggage: "
-              f"${self.cost_per_kilogram_of_baggage}")
+    def cost_kilogram_of_baggage(self) -> str:
+        """
+        Informs about the additional cost of baggage
 
-    def calculate_travel_time(self, distance):
+        Returns:
+        str: A string containing the cost per kilogram of baggage.
         """
-        The parameter accepts a distance for which the time to overcome
-        it in hours and minutes is calculated for this type of transport
-        """
-        # add 1 hour for check-in and security
-        travel_time = (distance / self.speed + 1) * 60
-        hours = travel_time // 60
-        minutes = travel_time % 60
-        return f"{round(hours)}h {round(minutes)}m"
+        return(f"The price of one kg of luggage: "
+               f"${self.cost_per_kilogram_of_baggage}")
 
 
 def get_transport_options(distance: float, train_compartment: Train,
-                          train_sitting: Train, airplane: Airplane):
+                          train_sitting: Train, airplane: Airplane) -> dict:
     """
-    The function takes an object of classes and a distance.
-    Initializes the work of each class for the given distance.
+    Returns a dictionary of options for different types of transportation
+    for the given distance.
+
+    Args:
+    distance (float): The distance of the trip in kilometers.
+    train_compartment (Train): A Train object representing a compartment train.
+    train_sitting (Train): A Train object representing a sitting train.
+    airplane (Airplane): An Airplane object representing an airplane.
+    Returns:
+    dict: A dictionary containing information about the travel time, cost, and
     """
-    train_compartment_time = train_compartment.calculate_travel_time(distance)
-    train_compartment_cost = train_compartment.get_travel_cost(distance)
+    train_compartment_time = train_compartment.calculate_travel_time(
+        distance)
+    train_compartment_travel_time = converting_time_to_hours_minutes(
+        train_compartment_time)
+    train_compartment_cost = train_compartment.get_travel_cost(
+        distance)
 
     train_sitting_time = train_sitting.calculate_travel_time(distance)
+    train_sitting_travel_time = converting_time_to_hours_minutes(train_sitting_time)
     train_sitting_cost = train_sitting.get_travel_cost(distance)
 
     airplane_time = airplane.calculate_travel_time(distance)
+    airplane_travel_time = converting_time_to_hours_minutes(airplane_time)
     airplane_cost = airplane.get_travel_cost(distance)
 
     options = {
-        "train compartment": {"time": train_compartment_time,
-                              "cost": train_compartment_cost},
-        "train sitting": {"time": train_sitting_time,
-                          "cost": train_sitting_cost},
-        "airplane": {"time": airplane_time, "cost": round(airplane_cost, 2)},
+        "train compartment": {"time": train_compartment_travel_time,
+                              "cost": train_compartment_cost,
+                              "info": train_compartment.display_info()},
+        "train sitting": {"time": train_sitting_travel_time,
+                          "cost": train_sitting_cost,
+                          "info": train_sitting.display_info()},
+        "airplane": {"time": airplane_travel_time,
+                     "cost": airplane_cost,
+                     "info": airplane.display_info()},
     }
 
     return options
 
 
-def get_input_float_from_user():
-    """ Accepts a distance from the user and validates the input """
+def converting_time_to_hours_minutes(travel_time: float) -> str:
+    """
+    Convert travel time in minutes to hours and minutes.
+
+    Args:
+    travel_time (int): The travel time in minutes.
+    Returns:
+    str: A string representation of the travel time in hours and minutes.
+    """
+    hours = travel_time // 60
+    minutes = travel_time % 60
+    return f"{round(hours)}h {round(minutes)}m"
+
+
+def get_input_float_from_user() -> float:
+    """
+    Get input distance in kilometers from the user and validate the input.
+
+    Returns:
+        float: The distance in kilometers entered by the user.
+    """
     while True:
         try:
             value = float(input("Enter the distance you need to cover (km): "))
             return value
         except ValueError:
-            print("Input Error! Enter the number")
+            print("Input Error! Enter a number.")
 
 
-def print_transport_option(options, key):
-    """ Displays trip data for the user """
+def print_transport_option(options: dict, key: str) -> None:
+    """
+    Print trip data for the user.
+
+    Args:
+    options (dict): A dictionary containing the different transport options.
+    key (str): The key of the transport option to display.
+    Returns:
+    None
+    """
     print("\n--------------------------\n")
     print(f"{key.capitalize()}:\nTime - {options[key]['time']} "
           f"\nCost - ${options[key]['cost']}")
     print("\nAdditional information:")
+    print(options[key]['info'])
 
 
-def main():
+def main() -> None:
     """
-    Creates class objects and for each class displays the calculated
-    information about the time and cost of the trip for the distance received
-    from the user, as well as additional information about the transport.
+    Compare travel time and cost of travel by different kinds of transport
+    for a given distance.
+
+    Returns:
+        None
     """
     train_compartment = Train(80, 14, "compartment", 40, 20)
     train_sitting = Train(100, 8, "sitting", 60)
@@ -184,13 +259,12 @@ def main():
 
     print("\n~ Transport options ~")
     print_transport_option(options, "train compartment")
-    train_compartment.display_info()
     print_transport_option(options, "train sitting")
     train_sitting.display_info()
     print_transport_option(options, "airplane")
-    airplane.display_info()
-    airplane.cost_kilogram_of_baggage()
+    print(airplane.cost_kilogram_of_baggage())
 
 
 if __name__ == "__main__":
     main()
+
